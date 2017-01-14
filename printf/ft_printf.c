060646e4a6ab1343289e2aa785ba0ffab873e7ab
Oleksiy Kres <alekseykres@gmail.com>
2017-01-14 17:33:00 +0200

    ...

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,13 +6,23 @@
 /*   By: okres <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/13 16:44:18 by okres             #+#    #+#             */
-/*   Updated: 2017/01/13 19:10:40 by okres            ###   ########.fr       */
+/*   Updated: 2017/01/14 17:32:41 by okres            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char * restrict format, ...)
+int ft_printf(const char * restrict str, ...)
 {
+	va_list vl;
+	char *ptr;
+
+	ptr = str;
+	va_start (vl, str);
+	while (ptr)
+	{
+		if (*ptr == '%')
+	}
+
 	return (0);
 }
