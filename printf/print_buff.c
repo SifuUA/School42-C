a277832a42fb32415649d6e209fe138da20eb767
Alex Kres <alekseykres@gmail.com>
2017-01-23 17:59:16 +0200

    new func

---
+++ printf/print_buff.c
@@ -0,0 +1,33 @@
+#include "ft_printf.h"
+
+int		get_width(t_pf *st)
+{
+	int w_str;
+	
+	if (st->width > st->precision && st->width > ft_strlen(st->buffer))
+		w_str = st->width;
+	else if (st->precision > st->width && st->precision > ft_strlen(st->buffer))
+		w_str = st->precision;
+	else
+		w_str = ft_strlen(st->buffer);
+	return(w_str);
+}
+
+
+int		print_buff(t_pf *st)
+{
+	int		str_w;
+	int		num_l;
+	int		space_c;
+
+	str_w = get_width(st);
+	num_l = ft_strlen(st->buffer);
+	space_c = str_w - num_l + st->precision;
+	while (space_c > 0)
+	{
+		ft_putchar(' ');
+		space_c--;
+	}
+
+
+}
