8dcf76b7e8e9925bb531f9cf2d2983a59a6004e9
okres <alekseykres@gmail.com>
2017-02-24 14:48:46 +0200

    read_from_arg_done

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,35 +1,46 @@
 #include "push_swap.h"
 
-static  void    write_in_list(t_d_linklst *list, char **s)
+static int      find_same(T num, t_d_linklst *list)
+{
+	t_node  *tmp;
+
+	tmp = list->head;
+	while (tmp)
+	{
+		if (tmp->value == num)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+static void     write_in_list(t_d_linklst *list, char **s)
 {
 	T   i;
-	T   j;
 	T   tmp;
 
 	i = 1;
 	tmp = 0;
 	while (s[i])
 	{
-		j = 0;
-		while (s[i][j])
+		tmp = ft_atoi(s[i]);
+		if (ft_strcmp(ft_itoa((int)tmp), s[i]) == 0 && find_same(tmp, list) == 0)
+			push_front(list, tmp);
+		else
 		{
-			tmp = ft_atoi(s[i]);
-			if (ft_strcmp(ft_itoa((int)tmp), s[i]) == 0)
-				push_front(list, tmp);
-			else
-				exit(1);
-			j++;
+			put("Error");
+			exit(2);
 		}
 		i++;
 	}
 }
 
-static  int     check_if_num(char **s)
+static int      check_if_num(char **s)
 {
 	T   i;
 	T   j;
 
-	i = 0;
+	i = 1;
 	while (s[i])
 	{
 		j = 0;
@@ -55,8 +66,13 @@ int             main(int argc, char **argv)
 	if (argc > 1)
 	{
 		if (check_if_num(argv))
-			exit (1);
+		{
+			put("Error");
+			exit(1);
+		}
 		write_in_list(list_a, argv);
 	}
+	else
+		write(1, "\n", 1);
 	return (0);
 }
